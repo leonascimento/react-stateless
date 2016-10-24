f746d57ac3c57a7b8999542bc8021f6e989ff3d6
Leonardo Nascimento <lnascimento@silentcircle.com>
2016-10-24 04:22:43 -0300

    transpiling

--- dist/index.js
+++ dist/index.js
@@ -1,35 +1,51 @@
-import React from 'react';
+'use strict';
 
-export function createClass(specification) {
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.createClass = createClass;
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function createClass(specification) {
     if (!specification instanceof Function && specification.render === undefined) {
         throw new Error('[ReactStateless.createClass(specification)] Not render function found. "specification" should be a render function or contain a render function.');
     }
 
-    let render = function () {
+    var render = function render() {
         return specification instanceof Function ? specification(this.props) : specification.render(this.props);
     };
 
-    let displayName;
+    var displayName = void 0;
     if (specification instanceof Function) displayName = specification.name || undefined;
     if (specification.render instanceof Function) displayName = specification.render.name || undefined;
 
-    let supportedLifecycleMethods = ['componentWillMount', 'componentDidMount', 'componentWillReceiveProps', 'shouldComponentUpdate', 'componentWillUpdate', 'componentDidUpdate', 'componentWillUnmount'];
+    var supportedLifecycleMethods = ['componentWillMount', 'componentDidMount', 'componentWillReceiveProps', 'shouldComponentUpdate', 'componentWillUpdate', 'componentDidUpdate', 'componentWillUnmount'];
 
-    let supportedProperties = ['propTypes', 'defaultProps', 'getDefaultProps', 'displayName'];
+    var supportedProperties = ['propTypes', 'defaultProps', 'getDefaultProps', 'displayName'];
 
-    let componentSpecification = { displayName };
+    var componentSpecification = { displayName: displayName };
 
-    supportedLifecycleMethods.filter(method => specification[method] !== undefined).forEach(method => {
+    supportedLifecycleMethods.filter(function (method) {
+        return specification[method] !== undefined;
+    }).forEach(function (method) {
         componentSpecification[method] = function () {
-            const args = [this.props].concat(arguments[0] === undefined ? [] : arguments[0]).concat(this.refs || []);
+            var args = [this.props].concat(arguments[0] === undefined ? [] : arguments[0]).concat(this.refs || []);
             return specification[method].apply(null, args);
         };
     });
 
-    supportedProperties.filter(prop => specification[prop] !== undefined).forEach(prop => componentSpecification[prop] = specification[prop]);
+    supportedProperties.filter(function (prop) {
+        return specification[prop] !== undefined;
+    }).forEach(function (prop) {
+        return componentSpecification[prop] = specification[prop];
+    });
 
     componentSpecification.render = render;
 
-    return React.createClass(componentSpecification);
+    return _react2.default.createClass(componentSpecification);
 }
-
